de571b2276b26a540b510a54c2bb591299827a08
Ekky Monerxy Fujianto <45175062+EkkyMonerxyF@users.noreply.github.com>
2020-11-02 13:22:30 +0700

    Lab 1

---
+++ TI08-Lab-1-EkkyMonerxyF/main.py
@@ -0,0 +1,14 @@
+# SOAL 1 - Menghitung rata-rata
+# Tuliskan program untuk Soal 1 di bawah ini
+num = eval(input("Masukkan bilangan pertama: "))
+num1 = eval(input("Masukkan bilangan kedua: "))
+num2 = eval(input("Masukkan bilangan ketiga: "))
+print("rata-rata bilangan", num, num1,"dan", num2, "adalah", num+num1+num2/3)
+
+
+
+
+# SOAL 2 - Menulis kelipatan bilangan
+# Tuliskan program untuk Soal 2 di bawah ini
+bilangan = int(input("Masukan sebuah bilangan bulat: "))
+print(bilangan, bilangan*2, bilangan*3, bilangan*4, bilangan*5, sep="-")
